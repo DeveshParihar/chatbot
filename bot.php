61675c499829d3244d927912a387126ae6e0bc39
Devesh Parihar <119911788+DeveshParihar@users.noreply.github.com>
2024-10-19 14:09:02 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost","root","","chatbots");
+$conn = mysqli_connect("localhost","root","","onlinebot");
 if($conn){
     $user_messages = mysqli_real_escape_string($conn, $_POST['messageValue']);
     
-    $query = "SELECT * FROM chatbots WHERE messages LIKE '%$user_messages%'";
+    $query = "SELECT * FROM chatbot WHERE messages LIKE '%$user_messages%'";
     $runQuery = mysqli_query($conn, $query);
     
     if(mysqli_num_rows($runQuery) > 0)
